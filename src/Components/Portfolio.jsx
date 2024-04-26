0888e40a286991409d8d90028d8b970bdb06e65c
Michał Wróblewski <157585200+Paperocean@users.noreply.github.com>
2024-04-26 16:59:33 +0000

    portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -45,7 +45,7 @@ const projectList = [
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+      "Includes my experience and abilities.",
     url: "https://paperocean.github.io/resume/",
   },
   {
